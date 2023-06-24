c296fdc4db1023b84633d6a384c2493f19a89bc8
Cyrille_Tony <56481146+cyrilletony@users.noreply.github.com>
2023-06-24 13:18:49 +0300

    modified:   Templates/admin/base/admin_base.html 	modified:   Templates/admin/base/auth_base.html 	modified:   Templates/admin/ndc/power_rooms.html 	modified:   settings/__pycache__/settings.cpython-38.pyc 	modified:   settings/settings.py

--- settings/settings.py
+++ settings/settings.py
@@ -10,6 +10,7 @@ app.config["TIMEZONE"] = "Africa/Nairobi"
 # app.config["HOST"] = "localhost.localdomain:5050"
 app.config["PORT"] = 5050
 # app.config["SERVER_NAME"] = "localhost.localdomain:5050"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://admin:AutoLinkAWS2022@autolink.crc1i8e8xwee.af-south-1.rds.amazonaws.com/ndc_daily_checklist"
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost/ndc_daily_checklist"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_POOL_SIZE"] = 50
@@ -18,3 +19,5 @@ app.config["SESSION_TYPE"] = "sqlalchemy"
 app.config["MAX_CONTENT_LENGTH"] = 10485760
 app.config["ALLOWED_EXTENSIONS"] = {'txt','pdf','png','jpg','jpeg','gif'}
 app.config["UPLOAD_FOLDER"] = "static/uploads"
+
+# BJegdw6e0(CS
